4d6957779e8988371122fecf904274b1ca54a37d
zakiia <zakiasultanabhuiyan@gmail.com>
2023-11-16 02:44:31 +0600

    first commit

---
+++ banking.js
@@ -0,0 +1,34 @@
+document.getElementById('deposit-button').addEventListener('click',function(){
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositText);
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = newDepositAmount + previousDepositAmount;
+    depositTotal.innerText = newDepositTotal;
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    depositInput.value = '';
+})
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    withdrawInput.value = '';
+})
+
---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    if(userEmail == 'shontan@baap.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html'
+    }
+}) 
